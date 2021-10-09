b25e0b301b21971b622541c63e55b7cb5fb0d692
Filip Mårtensson <fenwikk@protonmail.com>
2021-10-09 08:50:40 +0200

    Added Skill Tree Core System

---
+++ Assets/SkillTree/Models/Skill.cs
@@ -0,0 +1,8 @@
+[System.Serializable]
+public class Skill
+{
+    public int id;
+    public int[] dependencies;
+    public bool unlocked;
+    public int price;
+}
---
+++ Assets/SkillTree/Models/SkillTree.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+ 
+[System.Serializable]
+public class SkillTree {
+    public Skill[] skilltree;
+}
